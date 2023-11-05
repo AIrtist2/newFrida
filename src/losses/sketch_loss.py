08d58694315d6d80c454cd506218065215a05db5
AIrtist2 <149892061+AIrtist2@users.noreply.github.com>
2023-11-05 15:09:19 -0500

    Update sketch_loss.py

--- src/losses/sketch_loss.py
+++ src/losses/sketch_loss.py
@@ -112,7 +112,7 @@ try: # If running ros, get the painting code dir
     #path = os.path.join(ros_dir,'src','sketch_loss', 'pretrained', 'photosketch.pth')
     ##############
     path = path = 'sketch_loss/pretrained/photosketch.pth'
-except:
+#except:
     #path = os.path.join(ros_dir,'src','sketch_loss', 'pretrained', 'photosketch.pth')
     #path = 'sketch_loss/pretrained/photosketch.pth'
     #path = path = '/content/newFrida/sketch_loss/pretrained/photosketch.pth'
